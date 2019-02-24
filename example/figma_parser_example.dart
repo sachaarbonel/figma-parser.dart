934ca41577de0063d2c1075242b1ee1daab3d2e5
Sach97 <sacha.arbonel@hotmail.fr>
2019-02-24 13:15:56 +0100

    add utility function normalize

--- example/figma_parser_example.dart
+++ example/figma_parser_example.dart
@@ -32,17 +32,17 @@ class Coordinates {
       };
 }
 
-List<List<int>> group_runs(List<int> l, int threshold) {
+List<List<double>> group_runs(List<double> l, double threshold) {
   l.sort();
-  List<List<int>> out = <List<int>>[];
-  List<int> temp = <int>[];
+  List<List<double>> out = <List<double>>[];
+  List<double> temp = <double>[];
   temp.add(l[0]);
   for (var i = 0; i < l.length - 1; i++) {
-    if (l[i + 1] == l[i] + threshold) {
+    if (l[i + 1] <= l[i] + threshold) {
       temp.add(l[i + 1]);
     } else {
       out.add(temp);
-      temp = <int>[];
+      temp = <double>[];
       temp.add(l[i + 1]);
     }
   }
@@ -50,11 +50,24 @@ List<List<int>> group_runs(List<int> l, int threshold) {
   return out;
 }
 
+double normalize(int value, int min, int max) {
+  return 1 - ((value - min) / (max - min));
+}
+
 main(List<String> args) async {
   // String token = args[0];
   // String file_id = args[1];
   // String node_id = args[2];
-  // Figma figma = figmaFromFile(node_id);
-  List<int> listInts = [0, 1, 3, 4, 5, 10, 11, 12, 13, 20, 21, 22];
-  print(group_runs(listInts, 2));
+  //Figma figma = figmaFromFile(node_id);
+  List<int> listInts = [1010, 790, 810, 990, 590, 610];
+  listInts.sort();
+  print(listInts.reversed);
+  int _max = listInts.reduce(max);
+  int _min = listInts.reduce(min);
+  List<double> normalizedList = <double>[];
+  for (int i in listInts) {
+    normalizedList.add(normalize(i, _max, _min));
+  }
+  print(normalizedList.reversed);
+  print(group_runs(normalizedList, 0.1).reversed);
 }
